214d5d7167621787e64eb8ae75e9110e467263c9
Jung Ji Won <abiding77@naver.com>
2022-03-28 16:18:54 +0900

    CORS Error Handling

--- src/main/java/com/j2kb/valueup/WebMvcConfig.java
+++ src/main/java/com/j2kb/valueup/WebMvcConfig.java
@@ -1,14 +1,16 @@
 package com.j2kb.valueup;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://183.109.87.229:8080")
-                .allowedMethods("OPTIONS","GET","POST","PATCH","DELETE");
+                .allowedOrigins("*")
+                .allowedMethods("*");
     }
 }
